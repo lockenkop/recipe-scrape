ec0899ea25eba5ccc332de8c285541c8dca33584
j.fenchel <j.fenchel@senec.com>
2022-01-14 10:57:18 +0100

    template cahnges

--- chefkoch_server.py
+++ chefkoch_server.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("EINWAHL!")
     return render_template('base.html')
 
 @app.route("/", methods = ['POST'])
 def ingred_search():
+    print("searching")
     chefkoch = RecipeScraper()
     ingreds = request.form['ingreds']
     name_to_search = request.form['recipe_name']
@@ -21,4 +23,4 @@ def ingred_search():
     if name_to_search:  
         found_recipes = chefkoch.showRecipe(name_to_search)
         return render_template('show_urls.html', urls=found_recipes, name=name_to_search)
-    return name_to_search
+    return "NÖ"
